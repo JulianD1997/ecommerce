4d75eb672ac1216728c6461f249fc5335038da19
Julian <julian.casallasb@gmail.com>
2023-02-12 21:12:21 -0500

    form product

--- ecommerce/inventory/urls.py
+++ ecommerce/inventory/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('add_product/',views.add_product,name='add_product'),
-    path('add_supplier/',views.add_supplier,name='add_supplier'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('edit_product/<int:id>', views.edit_product, name='edit_product'),
+    path('add_supplier/', views.add_supplier, name='add_supplier'),
 ]
--- ecommerce/inventory/views.py
+++ ecommerce/inventory/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product,Supplier
+from .forms import ProductForm,SupplierForm
 
-def add_product(request):
-    return render(request,'product/add_product.html',{})
+def add_product(request): 
+    form = ProductForm()
+    context = {
+        'form':form,
+        'accion':'Agregar producto',
+    }
+    return render(request,'product/form_product.html',context)
+
 def add_supplier(request):
     return render(request,'supplier/add_supplier.html',{})
 
+def edit_product(request,id):
+    if(request.method == 'GET'):
+        product = Product.objects.get(id=id)
+        form = ProductForm(instance=product)
+        context = {
+            'accion':'Editar producto',
+            'form':form,
+        }
+        return render(request,'product/form_product.html',context)
